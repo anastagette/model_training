b51eee72c34bba0a81483978c4eb2d93f25c40db
anastagette <sharkunova.anastsiya@mail.ru>
2020-03-17 19:23:56 +1000

    add files to project

--- testing_multiprocess.py
+++ testing_multiprocess.py
@@ -2,32 +2,34 @@ import speech_recognition as sr
 import multiprocessing
 import time
 
+
 def record(conn, r):
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
-        audio = r.listen(source, timeout=2)
+        audio = r.listen(source, phrase_time_limit=2)
+        print('nayn')
         conn.send(audio)
         conn.close()
-
 
 def transcript(conn, r):
     while True:
         audio = conn.recv()
         transcript_str = r.recognize_google(audio, language='ru-RU')
         print (transcript_str)
-        conn.close()
-
+        break
 
 if __name__ == '__main__':
     r = sr.Recognizer()
     one_conn, two_conn = multiprocessing.Pipe()
 
-    p1 = multiprocessing.Process(target=record, args=(one_conn, r))
-    p2 = multiprocessing.Process(target=transcript, args=(two_conn, r))
-    p1.start()
-    p2.start()
-    p1.join()
-    p2.join()
+    while True:
+        p1 = multiprocessing.Process(target=record, args=(one_conn, r))
+        p2 = multiprocessing.Process(target=transcript, args=(two_conn, r))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+
 
 
 # import multiprocessing
