10da408495d52efba4342f4f0d47f499413dad7f
anastagette <62226869+anastagette@users.noreply.github.com>
2020-04-15 11:18:45 +1000

    Delete recursion1.py

--- recursion1.py
+++
@@ -1,9 +0,0 @@
-def rec_lineral_sum(some_list):
-    if not some_list:
-        return 0
-    else:
-        return some_list[0] + rec_lineral_sum(some_list[1:])
-
-some_list = [1.09, 2.05, 3.89]
-
-print(rec_lineral_sum(some_list))
