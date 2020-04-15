14325b6a1c417b09f271cc0f3811f442e8b4e127
anastagette <62226869+anastagette@users.noreply.github.com>
2020-04-15 11:18:36 +1000

    Delete offline_recognizer.py

--- offline_recognizer.py
+++
@@ -1,7 +0,0 @@
-import speech_recognition as sr
-
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    r.adjust_for_ambient_noise(source)
-    audio = r.listen(source, phrase_time_limit=3.0)
-print(r.recognize_sphinx(audio))
